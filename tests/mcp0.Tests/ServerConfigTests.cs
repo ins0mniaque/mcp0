90a12a85fe50c700141d3f9355b849af810c106a
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2025-04-05 16:04:14 -0400

    Add tests project

---
+++ tests/mcp0.Tests/ServerConfigTests.cs
@@ -0,0 +1,13 @@
+﻿namespace mcp0.Tests;
+
+[TestClass]
+public sealed class ServerConfigTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var config = new ServerConfig();
+
+        Assert.IsNotNull(config);
+    }
+}
