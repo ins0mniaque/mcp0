b2f3ca11d2a519c903a5b2c06a9b2bcd14d12d91
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2025-04-09 00:21:54 -0400

    Fix warning

--- src/mcp0/Core/McpClientExtensions.cs
+++ src/mcp0/Core/McpClientExtensions.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Logging;
 
 using ModelContextProtocol;
 using ModelContextProtocol.Client;
-using ModelContextProtocol.Protocol.Messages;
 using ModelContextProtocol.Protocol.Types;
 
 namespace mcp0.Core;
