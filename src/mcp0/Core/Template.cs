a87fa292229fffe11a74270dd3a891236759d31f
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2025-04-09 17:12:46 -0400

    Add template engine

---
+++ src/mcp0/Core/Template.cs
@@ -0,0 +1,42 @@
+using System.Text.RegularExpressions;
+
+namespace mcp0.Core;
+
+internal static partial class Template
+{
+    [GeneratedRegex(@"\{\{(?<name>[a-zA-Z_][a-zA-Z0-9_]+)(?<required>\??):?(?<description>[^\}]*)\}\}", RegexOptions.Compiled)]
+    private static partial Regex GenerateEngine();
+    private static readonly Regex engine = GenerateEngine();
+
+    internal record Argument(string Name, string? Description, bool IsRequired);
+
+    public static Argument[] Parse(string template)
+    {
+        return engine.Matches(template).Select(ToArgument).ToArray();
+
+        static Argument ToArgument(Match match)
+        {
+            return new(
+                match.Groups["name"].Value,
+                match.Groups["description"].Length is 0 ? null : match.Groups["description"].Value,
+                match.Groups["required"].Length is 0);
+        }
+    }
+
+    public static string Render(string template, IReadOnlyDictionary<string, string> arguments)
+    {
+        return engine.Replace(template, ReplaceArgument);
+
+        string ReplaceArgument(Match match)
+        {
+            var name = match.Groups["name"].Value;
+            if (arguments.TryGetValue(name, out var value))
+                return value;
+
+            if (match.Groups["required"].Length is 0)
+                return match.Value;
+
+            return string.Empty;
+        }
+    }
+}
