19940f7f486bd7578c9497787b0284e676a90ed1
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2025-04-14 21:45:11 -0400

    Fix JSON default values serialization

--- src/mcp0/Models/ModelContext.cs
+++ src/mcp0/Models/ModelContext.cs
@@ -2,11 +2,14 @@ using System.Text.Json.Serialization;
 
 namespace mcp0.Models;
 
-[JsonSourceGenerationOptions(WriteIndented = true, PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+[JsonSourceGenerationOptions(
+    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,
+    PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
+    WriteIndented = true)]
 [JsonSerializable(typeof(Configuration))]
 [JsonSerializable(typeof(Server))]
 [JsonSerializable(typeof(StdioServer))]
 [JsonSerializable(typeof(SseServer))]
 [JsonSerializable(typeof(string[]))]
 [JsonSerializable(typeof(Dictionary<string, string>))]
-internal sealed partial class ModelContext : JsonSerializerContext { }
+internal sealed partial class ModelContext : JsonSerializerContext;
