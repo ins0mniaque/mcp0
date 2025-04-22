bdedd408eeadd9343918a62faee36d719b11b007
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2025-04-22 18:23:18 -0400

    Add prompt converter array support

--- src/mcp0/Models/Converters/PromptConverter.cs
+++ src/mcp0/Models/Converters/PromptConverter.cs
@@ -8,7 +8,10 @@ internal sealed class PromptConverter : JsonConverter<Prompt>
     public override Prompt Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         if (reader.TokenType is JsonTokenType.String)
-            return Prompt.Parse(reader.GetString() ?? throw new JsonException("Expected a string or object for the prompt configuration"));
+            return Prompt.Parse(reader.GetString() ?? throw new JsonException("Expected a string, array or object for the prompt configuration"));
+
+        if (reader.TokenType is JsonTokenType.StartArray)
+            return new Prompt { Messages = reader.Deserialize(ModelContext.Default.PromptMessageArray) };
 
         return reader.Deserialize(ConverterContext.Default.Prompt);
     }
@@ -17,6 +20,8 @@ internal sealed class PromptConverter : JsonConverter<Prompt>
     {
         if (Prompt.TryFormat(prompt) is { } formatted)
             writer.WriteStringValue(formatted);
+        else if (prompt.Description is null && prompt.Options is null)
+            JsonSerializer.Serialize(writer, prompt.Messages, ConverterContext.Default.PromptMessageArray);
         else
             JsonSerializer.Serialize(writer, prompt, ConverterContext.Default.Prompt);
     }
