53ce3267cdb250c8d8e7b9d15db68ac49607ba90
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2025-04-22 23:00:49 -0400

    Add prompt message options string format

--- src/mcp0/Models/PromptMessage.cs
+++ src/mcp0/Models/PromptMessage.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 using mcp0.Core;
 
 namespace mcp0.Models;
@@ -21,22 +23,67 @@ internal sealed record PromptMessage
 
         return new()
         {
+            Options = TryParseOptions(Formattable.ParseAtEnd(ref text, " #")),
             ReturnArgument = Formattable.ParseAtEnd(ref text, "=>")?.TrimStart('{').TrimEnd('}'),
             Template = text
         };
     }
 
+    private static PromptOptions? TryParseOptions(string? formatted)
+    {
+        if (formatted is null)
+            return null;
+
+        var models = new List<string>();
+        var temperature = (float?)null;
+        foreach (var option in formatted.Split(' ', StringSplitOptions.TrimEntries))
+        {
+            if (option.Length is 0)
+                continue;
+            if (option[^1] is '%' && float.TryParse(option[..^1], CultureInfo.InvariantCulture, out var percentage))
+                temperature = percentage / 100f;
+            else
+                models.Add(option);
+        }
+
+        if (models.Count is 0 && temperature is null)
+            return null;
+
+        return new()
+        {
+            Model = models.ToArray(),
+            Temperature = temperature
+        };
+    }
+
     public static string? TryFormat(PromptMessage message)
     {
-        var options = message.Options;
-        var formattable = options is null;
-        if (!formattable)
+        if (!TryFormat(message.Options, out var formattedOptions))
             return null;
 
         var formatted = message.Template;
+        if (formattedOptions is not null)
+            formatted = Formattable.Format(formatted, formattedOptions, " # ");
         if (message.ReturnArgument is not null)
             formatted = Formattable.Format(formatted, $"{{{{{message.ReturnArgument}}}}}", " => ");
 
         return formatted;
+    }
+
+    private static bool TryFormat(PromptOptions? options, out string? formatted)
+    {
+        formatted = null;
+        if (options is null)
+            return true;
+
+        if (options.Context is not null || options.SystemPrompt is not null ||
+            options.MaxTokens is not null || options.StopSequences is not null)
+            return false;
+
+        formatted = string.Join(' ', options.Model ?? []);
+        if (options.Temperature is { } temperature)
+            formatted += ' ' + temperature.ToString("P0", CultureInfo.InvariantCulture);
+
+        return true;
     }
 }
